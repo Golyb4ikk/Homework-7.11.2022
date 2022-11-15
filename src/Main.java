6660daa0d40101a08d2fbb8c8c59b80c3c90bf09
golyb4ik <only_game2@mail.ru>
2022-11-15 13:51:40 +0300

    Задание 1.4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8+4-3.5;
-        var cat = 3.6+4-1.6;
-        var paper = 763789+4-7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend=friend+2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
     }
 }
