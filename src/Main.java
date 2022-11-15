5a9a08c59928b97be7de00b20915e6cfa86a417d
golyb4ik <only_game2@mail.ru>
2022-11-15 14:16:17 +0300

    Задание 1.7

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         var boxerWeight1=78.2;
         var boxerWeight2=82.7;
-        var sumWeight=boxerWeight1+boxerWeight2;
-        System.out.println(sumWeight);
-        var raznicaWeight=boxerWeight2 % boxerWeight1;
+        var raznicaWeight=boxerWeight2 - boxerWeight1;
         System.out.println(raznicaWeight);
+        var raznicaWeight1=boxerWeight2 % boxerWeight1;
+        System.out.println(raznicaWeight1);
     }
 }
