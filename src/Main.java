eafb91cec857712a7a2b4ff926ac4ea90b143944
golyb4ik <only_game2@mail.ru>
2022-11-15 14:24:12 +0300

    Задание 1.8

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        var boxerWeight1=78.2;
-        var boxerWeight2=82.7;
-        var raznicaWeight=boxerWeight2 - boxerWeight1;
-        System.out.println(raznicaWeight);
-        var raznicaWeight1=boxerWeight2 % boxerWeight1;
-        System.out.println(raznicaWeight1);
+        var allWork=640;
+        var timeEveryone=8;
+        var emp=allWork/timeEveryone;
+        System.out.println("Всего работников в компании-"+emp+" человек");
+        emp=emp+94;
+        allWork=timeEveryone*emp;
+        System.out.println("Если в компании работает "+emp+" человека,то всего "+allWork+" часа работы может быть поделено между сотрудниками");
     }
 }
